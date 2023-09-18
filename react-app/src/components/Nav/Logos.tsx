86cae75f58a47299879fd979bb1c203db485cd26
Tyler Auth <76677891+ttauth@users.noreply.github.com>
2023-09-18 03:08:07 -0500

    I have the layout figured out, I just need to add logos

---
+++ react-app/src/components/Nav/Logos.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { AiOutlineGithub } from 'react-icons/fa';
+
+class Logos extends Component {
+    render() {
+        return <AiOutlineGithub />;
+    }
+}
+
+export default Logos;
